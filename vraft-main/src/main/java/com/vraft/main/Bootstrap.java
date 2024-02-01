3ae985f4ab4e0d910f630e037c260a1ec65b9a9b
jweihsz <jweih.hjw@alibaba-inc.com>
2024-02-01 10:33:02 +0800

    feat:add log4j2 configuration

--- vraft-main/src/main/java/com/vraft/main/Bootstrap.java
+++ vraft-main/src/main/java/com/vraft/main/Bootstrap.java
@@ -1,7 +1,12 @@
 package com.vraft.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Bootstrap {
+    private final static Logger logger = LogManager.getLogger(Bootstrap.class);
+
     public static void main(String[] args) throws Exception {
-        System.out.println("hello world!");
+        logger.info("hello world!");
     }
 }
