6c7400a898a73ffca8ccdbbf369f9be11f331d09
jweihsz <1989428128@qq.com>
2024-02-18 20:02:04 +0800

    feat:add thread pool

---
+++ vraft-core/src/main/java/com/vraft/core/pool/ThreadPool.java
@@ -0,0 +1,26 @@
+package com.vraft.core.pool;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import com.vraft.core.utils.SystemUtil;
+
+/**
+ * @author jweihsz
+ * @version 2024/2/18 19:39
+ **/
+public class ThreadPool {
+    private ThreadPool() {}
+
+    private final static int keepAlive = 30;
+    public final static ThreadPoolExecutor ACTOR;
+    private final static int cpuNum = SystemUtil.getPhyCpuNum();
+
+    static {
+        ACTOR = new ThreadPoolExecutor(
+            cpuNum, cpuNum, keepAlive,
+            TimeUnit.MINUTES, new LinkedBlockingQueue<>());
+    }
+
+}
