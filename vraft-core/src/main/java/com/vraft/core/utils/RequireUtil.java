2802c55d5ad653dbce043ddcebd04ca3d3f58a5d
jweihsz <1989428128@qq.com>
2024-03-11 11:52:59 +0800

    add require util

---
+++ vraft-core/src/main/java/com/vraft/core/utils/RequireUtil.java
@@ -0,0 +1,29 @@
+package com.vraft.core.utils;
+
+/**
+ * @author jweihsz
+ * @version 2024/3/11 11:49
+ **/
+public class RequireUtil {
+    private RequireUtil() {}
+
+    public static <T> T nonNull(T obj) {
+        if (obj != null) {return obj;}
+        throw new NullPointerException();
+    }
+
+    public static <T> T nonNull(T obj, String message) {
+        if (obj != null) {return obj;}
+        throw new NullPointerException(message);
+    }
+
+    public static void True(boolean exp) {
+        if (exp) {return;}
+        throw new IllegalArgumentException();
+    }
+
+    public static void True(boolean exp, Object msg) {
+        if (exp) {return;}
+        throw new IllegalArgumentException(String.valueOf(msg));
+    }
+}
