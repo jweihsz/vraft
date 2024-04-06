1ca4033dc43c7791fb28ece4d0dc2ba372ead8e3
jweihsz <1989428128@qq.com>
2024-04-06 17:48:30 +0800

    feat:init rocksdb store

---
+++ vraft-facade/src/main/java/com/vraft/facade/raft/logs/RaftLogsStore.java
@@ -0,0 +1,10 @@
+package com.vraft.facade.raft.logs;
+
+import com.vraft.facade.common.LifeCycle;
+
+/**
+ * @author jweihsz
+ * @version 2024/4/6 17:28
+ **/
+public interface RaftLogsStore extends LifeCycle {
+}
